fcd1695c81255e1ccf32240658e1f9364122726c
Harry <harry@lianghuangdeMBP.lan>
2019-03-21 10:22:49 +0800

    测试handle

--- backup.js
+++ backup.js
@@ -1,12 +1,3 @@
 var exec = require('child_process').exec
-exec('rm -rf static/_nuxt && mv .nuxt/dist/client static/_nuxt', function(
-  error,
-  stdout,
-  stderr
-) {
-  if (error) {
-    console.log(error)
-  } else {
-    console.log('成功')
-  }
-})
+console.log('--------post-deploy')
+exec('rm -rf static/_nuxt && mv .nuxt/dist/client static/_nuxt')
