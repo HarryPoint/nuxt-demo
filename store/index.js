9d8ed935a821ea32326311fe63f9332759bd9ed7
Harry <harry@lianghuangdeMBP.lan>
2019-03-22 16:19:15 +0800

    删除代码

--- store/index.js
+++
@@ -1,7 +0,0 @@
-export const actions = {
-  async nuxtServerInit({ commit, getters }, { req, route, redirect }) {
-    // commit('ua/SET_USER_AGENT', req.headers['user-agent'] || '')
-    // commit('ua/SET_INITIAL_HOST', req.headers['host'] || '')
-    //  判断是不是PC访问
-  }
-}
